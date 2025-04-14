a9b154078e241bee3456455e2762f6902fa9222c
rembov <i7888@mail.ru>
2025-04-14 19:52:43 +0300

    lab55

--- lab5/5lab.py
+++ lab5/5lab.py
@@ -1,12 +1,3 @@
-"""
-Классификатор погон ВС РФ по родам войск
-Предположим, человек пытается
-разберись, кто важнее...
-...он - или какой-то другой военнослужащий.
-В армии таких сомнений нет.
-Просто посмотрите на погоны
-и не стоит волноваться
-"""
 import os
 os.environ['KMP_DUPLICATE_LIB_OK'] = 'TRUE'
 import torch 
@@ -14,90 +5,176 @@ import torch.nn as nn
 import torchvision
 import torchvision.transforms as transforms
 import numpy as np
-import matplotlib.pyplot as plt
+
 device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+
+# Определение трансформаций для данных
+data_transforms = transforms.Compose([
+    transforms.Resize(68),
+    transforms.CenterCrop(64),
+    transforms.ToTensor()
+])
+
+print('1')
+train_dataset = torchvision.datasets.ImageFolder(root='./data/train', transform=data_transforms)
+test_dataset = torchvision.datasets.ImageFolder(root='./data/test', transform=data_transforms)
+class_names = train_dataset.classes
+batch_size = 4
+train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=16)
+test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=batch_size, shuffle=False, num_workers=16)
+print('1')
+
+# Определение модели
+class CnNet(nn.Module):
+    def __init__(self, num_classes=3):
+        super(CnNet, self).__init__()
+        self.layer1 = nn.Sequential(
+            nn.Conv2d(3, 16, kernel_size=7, stride=1, padding=2),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2))
+        self.layer2 = nn.Sequential(
+            nn.Conv2d(16, 32, kernel_size=5, stride=1, padding=2),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2))
+        self.layer3 = nn.Sequential(
+            nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=2),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2))
+        self.fc = nn.Linear(8*8*64, num_classes)
+        
+    def forward(self, x):
+        out = self.layer1(x)
+        out = self.layer2(out)
+        out = self.layer3(out)
+        out = out.reshape(out.size(0), -1)
+        out = self.fc(out)
+        return out
+
+print('1')
+num_classes = 3
+net = CnNet(num_classes=num_classes).to(device)
+lossFn = nn.CrossEntropyLoss()
+optimizer = torch.optim.SGD(net.parameters(), lr=0.01)
+num_epochs = 10
+save_loss = []
+print('1')
+
+# Обучение модели
+for epoch in range(num_epochs):
+    for i, (images, labels) in enumerate(train_loader):
+        images = images.to(device)
+        labels = labels.to(device)
+        outputs = net(images)
+        loss = lossFn(outputs, labels)
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        save_loss.append(loss.item())
+        if i % 10 == 0:  # Выводим лог каждые 10 шагов
+            print(f'Эпоха {epoch} из {num_epochs}, Шаг {i}, Ошибка: {loss.item()}')
+
+print('1')
+
+# Оценка точности модели
+correct_predictions = 0
+num_test_samples = len(test_dataset)
+with torch.no_grad():
+    for images, labels in test_loader:
+        images = images.to(device)
+        labels = labels.to(device)
+        pred = net(images)
+        _, pred_class = torch.max(pred.data, 1)
+        correct_predictions += (pred_class == labels).sum().item()
+
+accuracy = 100 * correct_predictions / num_test_samples
+print(f'Точность модели: {accuracy}%')
+torch.save(net.state_dict(), 'CnNet_cats_dogs_birds.ckpt')
+
+# Переход к AlexNet
 data_transforms = transforms.Compose([
     transforms.Resize(256),
     transforms.CenterCrop(224),
     transforms.ToTensor(),
-    transforms.Normalize(mean=[0.485, 0.456, 0.406],
-                        std=[0.229, 0.224, 0.225])
+    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
 ])
-train_dataset = torchvision.datasets.ImageFolder(root='./data/train',
-                                              transform=data_transforms)
-test_dataset = torchvision.datasets.ImageFolder(root='./data/test',
-                                             transform=data_transforms)
-class_names = train_dataset.classes
-print("Классы:", class_names)
-batch_size = 16
-train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size, 
-                                         shuffle=True, num_workers=2)
-test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=batch_size, 
-                                        shuffle=False, num_workers=2)
-net = torchvision.models.resnet18(pretrained=True)
+
+train_dataset = torchvision.datasets.ImageFolder(root='./data/train', transform=data_transforms)
+test_dataset = torchvision.datasets.ImageFolder(root='./data/test', transform=data_transforms)
+batch_size = 4
+train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=0)
+test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=batch_size, shuffle=False, num_workers=0)
+
+net = torchvision.models.alexnet(pretrained=True)
 for param in net.parameters():
     param.requires_grad = False
+
 num_classes = 3
-net.fc = nn.Linear(net.fc.in_features, num_classes)
+new_classifier = net.classifier[:-1]
+new_classifier.add_module('fc', nn.Linear(4096, num_classes))
+net.classifier = new_classifier
 net = net.to(device)
-criterion = nn.CrossEntropyLoss()
-optimizer = torch.optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
-num_epochs = 100
-save_loss = []
-for epoch in range(num_epochs):
-    running_loss = 0.0
-    for i, (inputs, labels) in enumerate(train_loader):
-        inputs = inputs.to(device)
+
+print('1')
+
+# Оценка точности до обучения
+correct_predictions = 0
+num_test_samples = len(test_dataset)
+with torch.no_grad():
+    for images, labels in test_loader:
+        images = images.to(device)
         labels = labels.to(device)
-        
+        pred = net(images)
+        _, pred_class = torch.max(pred.data, 1)
+        correct_predictions += (pred_class == labels).sum().item()
+
+accuracy = 100 * correct_predictions / num_test_samples
+print(f'Точность модели до обучения: {accuracy}%')
+
+# Дообучение AlexNet
+lossFn = nn.CrossEntropyLoss()
+optimizer = torch.optim.SGD(net.parameters(), lr=0.01)
+num_epochs = 10
+save_loss = []
+
+for epoch in range(num_epochs):
+    for i, (images, labels) in enumerate(train_loader):
+        images = images.to(device)
+        labels = labels.to(device)
+        outputs = net(images)
+        loss = lossFn(outputs, labels)
         optimizer.zero_grad()
-        
-        outputs = net(inputs)
-        loss = criterion(outputs, labels)
         loss.backward()
         optimizer.step()
-        
-        running_loss += loss.item()
         save_loss.append(loss.item())
-        
-        if i % 10 == 9:
-            print(f'Эпоха {epoch + 1}, шаг {i + 1}, loss: {running_loss / 10:.3f}')
-            running_loss = 0.0
-plt.figure(figsize=(10, 5))
-plt.plot(save_loss)
-plt.title('График функции потерь')
-plt.xlabel('Итерации')
-plt.ylabel('Loss')
-plt.show()
-correct = 0
-total = 0
-with torch.no_grad():
-    for inputs, labels in test_loader:
-        inputs = inputs.to(device)
-        labels = labels.to(device)
-        outputs = net(inputs)
-        _, predicted = torch.max(outputs.data, 1)
-        total += labels.size(0)
-        correct += (predicted == labels).sum().item()
+        if i % 10 == 0:  # Выводим лог каждые 10 шагов
+            print(f'Эпоха {epoch} из {num_epochs}, Шаг {i}, Ошибка: {loss.item()}')
 
-print(f'Точность на тестовом наборе: {100 * correct / total:.2f}%')
-torch.save(net.state_dict(), 'shoulder_straps_classifier.pt')
-def imshow(inp, title=None):
-    inp = inp.numpy().transpose((1, 2, 0))
+print('1')
+
+# Оценка точности после обучения
+correct_predictions = 0
+num_test_samples = len(test_dataset)
+with torch.no_grad():
+    for images, labels in test_loader:
+        images = images.to(device)
+        labels = labels.to(device)
+        pred = net(images)
+        _, pred_class = torch.max(pred.data, 1)
+        correct_predictions += (pred_class == labels).sum().item()
+
+accuracy = 100 * correct_predictions / num_test_samples
+print(f'Точность модели после обучения: {accuracy}%')
+
+# Визуализация результатов текстом
+inputs, classes = next(iter(test_loader))
+pred = net(inputs.to(device))
+_, pred_class = torch.max(pred.data, 1)
+
+for i, j in zip(inputs, pred_class):
+    img = i.numpy().transpose((1, 2, 0))
     mean = np.array([0.485, 0.456, 0.406])
     std = np.array([0.229, 0.224, 0.225])
-    inp = std * inp + mean
-    inp = np.clip(inp, 0, 1)
-    plt.imshow(inp)
-    if title is not None:
-        plt.title(title)
-    plt.pause(0.001)
-inputs, classes = next(iter(test_loader))
-outputs = net(inputs.to(device))
-_, preds = torch.max(outputs, 1)
-plt.figure(figsize=(12, 8))
-for i in range(min(8, inputs.size()[0])):
-    plt.subplot(2, 4, i+1)
-    imshow(inputs[i], f'Прогноз: {class_names[preds[i]]}\nРеальный: {class_names[classes[i]]}')
-plt.tight_layout()
-plt.show()
+    img = std * img + mean
+    img = np.clip(img, 0, 1)
+    print(f"Изображение классифицировано как: {class_names[j]}")
+print('1')
